3ed610a4cd084fa653af2d92ce68e44cb6c10cd6
FalseR20 <kno3.2002@gmail.com>
2022-02-28 11:49:57 +0300

    update ProgLang lab2 code

--- ProgLang/2/src/main.cpp
+++ ProgLang/2/src/main.cpp
@@ -5,31 +5,37 @@
 using namespace std;
 
 const char FILENAME[] = "..\\files\\Lorem.txt";
+//const char FILENAME[] = "..\\files\\Alphabet.txt";
 const char FILENAME_NEW[] = "..\\files\\NewFile.txt";
-const long long BUFFER_SIZE = 1024;
+const long long BUFFER_SIZE = 1023;
+char buffer[BUFFER_SIZE + 1];
 
 
 int main() {
     ifstream file(FILENAME, ios_base::ate | ios_base::binary);
-    ofstream new_file(FILENAME_NEW, ios_base::trunc | ios_base::binary);
-    if (!file.is_open() or !new_file.is_open()) {
+    ofstream fileNew(FILENAME_NEW, ios_base::trunc | ios_base::binary);
+
+    if (!file.is_open() or !fileNew.is_open()) {
         cout << "Some of files are not found";
         return -1;
     }
 
-    char buffer[BUFFER_SIZE];
-    const long long filesize = file.tellg();
-    long long cursor = filesize - (BUFFER_SIZE - 1);
+    long long cursor = file.tellg() - BUFFER_SIZE;  // Курсор - номер позиции в файле = BUFFER_SIZE с конца
     while (cursor > 0) {
-        file.seekg(cursor, ios_base::beg);
-        file.read(buffer, BUFFER_SIZE - 1);
-        reverse(buffer, buffer + (BUFFER_SIZE - 1));
-        new_file << buffer;
-        cursor -= (BUFFER_SIZE - 1);
+        file.seekg(cursor, ios_base::beg);  // Перенос указателя в потоке файла на курсор
+        file.read(buffer, BUFFER_SIZE);  // Чтение в буфер BUFFER_SIZE символов
+        reverse(buffer, buffer + BUFFER_SIZE);  // Поворот буфера
+        fileNew << buffer;  // Запись в новый файл
+        cursor -= BUFFER_SIZE;  // Смещение курсора
     }
-    file.seekg(ios_base::beg);
-    file.read(buffer, BUFFER_SIZE - 1 + cursor);
-    buffer[BUFFER_SIZE - 1 + cursor] = '\0';
-    reverse(buffer, buffer + (BUFFER_SIZE - 1) + cursor);
-    new_file << buffer;
+    cursor += BUFFER_SIZE;  // Теперь курсор - это конец: запись с 0 до cursor
+
+    file.seekg(ios_base::beg);  // Перенос указателя в потоке файла на начало
+    file.read(buffer, cursor);  // Чтение в буфер cursor символов
+    buffer[cursor] = '\0';  // Заполяется не весь буфер, поэтому надо отделить
+    reverse(buffer, buffer + cursor);
+    fileNew << buffer;
+
+    file.close();
+    fileNew.close();
 }
