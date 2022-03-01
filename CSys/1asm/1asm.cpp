6a4d2426aeb6c391911c53f9cc64f70b53676185
FalseR20 <kno3.2002@gmail.com>
2022-03-01 22:30:32 +0300

    CSys lab1 works

--- CSys/1asm/1asm.cpp
+++ CSys/1asm/1asm.cpp
@@ -4,11 +4,36 @@ using namespace std;
 
 
 int main() {
-	int a = 5;
+	char text[]     = "OhHelloMyWorld";
+	char text_new[] = "              ";
+
+	unsigned int len_ = strlen(text);
+	unsigned int half = len_ / 2;
+	unsigned int half2 = len_ - len_ / 2;
 
 	__asm {
-		add a, 2
-	}
+			mov  esi, 0
+			mov  edi, half2
+			mov  ecx, half
+		forbegin1:
+			mov  al, byte ptr text[esi]
+			mov  byte ptr text_new[edi], al
+			inc  esi
+			inc  edi
+			loop forbegin1
 
-	cout << a;
+			mov  esi, half
+			mov  edi, 0
+			mov  ecx, half2
+		forbegin2:
+			mov  al, byte ptr text[esi]
+			mov  byte ptr text_new[edi], al
+			inc  esi
+			inc  edi
+			loop forbegin2
+	}
+	cout << " Input: " << text << '\n';
+	cout << "Output: " << text_new;
+
+	return 0;
 }
