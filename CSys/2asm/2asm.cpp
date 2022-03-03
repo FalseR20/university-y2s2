535c573271f03226ad1e6062e37bac7c8480baff
FalseR20 <kno3.2002@gmail.com>
2022-03-03 16:42:48 +0300

    CSys lab2 add 1st and 2nd ways

--- CSys/2asm/2asm.cpp
+++ CSys/2asm/2asm.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	const char textin[] = "Этот текст имеет скрытый 2ой смысл. Ахахаха 435346426 а стоп...";
-	// const char textin[] = "У этого текста 20 значений. Смысла немного после точки";
+	// const char textin[] = "This text will be changed in the 1st way; abcxyz. 837467 aahahhahaha stop...";
+	const char textin[] = "2020, 2021 and 2022 are the worst years.";
 	char textout[100];
 
 	unsigned int len_ = strchr(textin, '.') - textin;
@@ -13,6 +13,8 @@ int main()
 
 	unsigned short is_prev_dec = 0;
 	unsigned short bo = 0;
+	unsigned short path = 0;
+	char prev_liter = '\0';
 
 	__asm {
 		mov ecx, len_; Прохождение всей длины textin
@@ -21,9 +23,9 @@ int main()
 	scan_lower:
 		cmp is_prev_dec, 0
 		je scan_digit
-		cmp textin[esi], 'а'
+		cmp textin[esi], 'a'
 		jb event2
-		cmp textin[esi], 'я'
+		cmp textin[esi], 'z'
 		ja event2
 		mov is_prev_dec, 0
 	scan_digit:
@@ -37,13 +39,47 @@ int main()
 		loop scan_lower
 
 	event1:
+		mov path, 1
+		mov ecx, len_
+		mov esi, 0
+	event1for:
+		mov al, textin[esi]
+		cmp textin[esi], 'a'
+		jb event1contin
+		cmp textin[esi], 'z'
+		ja event1contin
+		mov al, 'z'
+		sub al, textin[esi]
+		add al, 'a'
+		
+	event1contin:
+		mov textout[esi], al
+		inc esi
+		loop event1for
 
-		mov textout[0], 'Y'
 		jmp endprogram
 	event2:
-		mov textout[1], 'N'
+		mov path, 2
+		mov ecx, len_
+		mov esi, 0
+		mov edi, 0
+	event2for:
+		mov al, prev_liter
+		cmp al, textin[esi]
+		
+		je event2contin
+		mov al, textin[esi]
+		mov textout[edi], al
+		inc edi
+		mov prev_liter, al
+
+	event2contin:
+		inc esi
+		loop event2for
+		mov textout[edi], '\0'
+
 	endprogram:
 	}
 
-	cout << textout;
+	cout << "path " << path << ": " << textout;
 }
