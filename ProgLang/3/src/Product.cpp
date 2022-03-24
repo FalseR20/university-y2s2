6c9302cd9b51da6d9badc0004bd3fafea4998be4
FalseR20 <kno3.2002@gmail.com>
2022-03-24 20:51:46 +0300

    feat(ProgLang lab3): add Report.docx, update code fis((ProgLang lab3): CMakeLists.txt

--- ProgLang/3/src/Product.cpp
+++ ProgLang/3/src/Product.cpp
@@ -7,28 +7,28 @@
 Product::Product() : count_(0), price_(0) {
     name_ = new char[8];
     strcpy(name_, "Unnamed");
-    printf("Product \"%s\" created (without parameters)\n", name_);
+    printf("LOG: Product \"%s\" created (without parameters)\n", name_);
 }
 
-Product::Product(const char *name, const int count, const float price) : count_(count), price_(price) {
+Product::Product(const char *name, int count, float price) : count_(count), price_(price) {
     name_ = new char[strlen(name)];
     strcpy(name_, name);
-    printf("Product \"%s\" created (with parameters)\n", name_);
+    printf("LOG: Product \"%s\" created (with parameters)\n", name_);
 }
 
 Product::Product(const Product &product) : count_(product.count_), price_(product.price_) {
     name_ = new char[strlen(product.name_)];
     strcpy(name_, product.name_);
-    printf("Product \"%s\" created (by coping)\n", name_);
+    printf("LOG: Product \"%s\" created (by coping)\n", name_);
 }
 
 Product::~Product() {
-    printf("Product \"%s\" deleted\n", name_);
+    printf("LOG: Product \"%s\" deleted\n", name_);
     delete[] name_;
 }
 
 void Product::print() const {
-    printf("Product \"%s\": count = %d, price = %.2f\n", name_, count_, price_);
+    printf("LOG: Product \"%s\": count = %d, price = %.2f\n", name_, count_, price_);
 }
 
 char *Product::getName() const {
@@ -46,19 +46,18 @@ float Product::getPrice() const {
 }
 
 void Product::setName(const char *name) {
-    printf("Product \"%s\": changed name(%s -> %s)\n", name_, name_, name);
+    printf("LOG: Product \"%s\": changed name (%s -> %s)\n", name_, name_, name);
     delete[] name_;
     name_ = new char[strlen(name)];
     strcpy(name_, name);
 }
 
-void Product::setCount(const int count) {
-    printf("Product \"%s\": changed count(%d -> %d)\n", name_, count_, count);
+void Product::setCount(int count) {
+    printf("LOG: Product \"%s\": changed count (%d -> %d)\n", name_, count_, count);
     count_ = count;
 }
 
-void Product::setPrice(const float price) {
-    printf("Product \"%s\": changed price (%.2f -> %.2f)\n", name_, price_, price);
+void Product::setPrice(float price) {
+    printf("LOG: Product \"%s\": changed price (%.2f -> %.2f)\n", name_, price_, price);
     price_ = price;
 }
-
--- ProgLang/3/src/main.cpp
+++ ProgLang/3/src/main.cpp
@@ -7,20 +7,17 @@ int main() {
     auto empty = new Product;
     delete empty;
 
-    auto beerPtr = new Product("Ideal", 35, 1.29 /* from groshik*/ );
+    auto beerPtr = new Product("Ideal", 35, 1.29 /* from groshik */ );
     Product beerCopy(*beerPtr);
     delete beerPtr;
     beerCopy.setName(strcat(beerCopy.getName(), " NEW!!!"));
-    beerCopy.setPrice(beerCopy.getPrice() * 1.45);
+    beerCopy.setPrice(beerCopy.getPrice() * 1.45f);
 
-    Product chips("Lay's: Chile lemon", 10, 2.39);
+    Product chips("Lay's: Chili & lime", 10, 2.39);
     chips.setCount(15);
 
-    float averagePrice = (
-            (chips.getPrice() * chips.getCount() + beerCopy.getPrice() * beerCopy.getCount())
-            / (chips.getCount() + beerCopy.getCount())
-    );
-    printf("The average price = %.2f\n", averagePrice);
+    float starterPack = chips.getPrice() + beerCopy.getPrice();
+    printf("The minimum price = %.2f\n", starterPack);
 
     void (Product::*printPtr)() const;
     printPtr = &Product::print;
