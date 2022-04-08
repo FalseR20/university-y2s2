a9ac312f1699c3bd5d49119944e5097750f79714
FalseR20 <kno3.2002@gmail.com>
2022-04-08 18:20:58 +0300

    feat(CSys lab3): add lab as asm insert

---
+++ CSys/3asm/2asm.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void change_asm(const char*, const char);
+
+int main()
+{
+    const char text[100] = "This sentence has a lot of sense.";
+    const char letter = 's';
+    change_asm(text, letter);
+}
+
+void change_asm(const char* text, const char letter) {
+    char textin[100];
+    char textout[100];
+    strcpy(textin, text);
+    int len_ = strlen(text);
+    cout << " input: " << textin << "\n";
+
+    __asm {
+            mov ecx, len_
+            mov esi, 0
+            mov edi, 0
+
+        forbegin:
+            cmp textin[esi], ' '
+            jne skipnext
+            mov al, textin[esi + 1]
+            cmp al, letter
+            jne skipnext
+        skipword:
+            inc esi
+            dec ecx
+            cmp textin[esi], ' '
+            je forbegin
+            cmp textin[esi], '.'
+            je forbegin
+            jmp skipword
+
+        skipnext:
+            mov al, textin[esi]
+            mov textout[edi], al
+            inc edi
+            inc esi
+            loop forbegin
+
+            mov textout[edi], '\0'
+    }
+
+    cout << "output: " << textout << "\n";
+}
