e772b8bcd6a3f1fa6a98bdd3a20c1ea7a0347440
FalseR20 <kno3.2002@gmail.com>
2022-05-29 15:47:20 +0300

    feat(OS 6): completed

--- OS/6/main.c
+++ OS/6/main.c
@@ -1,75 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <sys/shm.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <signal.h>
 
-#define SHMNAME "my_shm"
+#define SHM_NAME "shared_memory"
+#define SIZE 1024
 
-void dochernij();
+void handler();
 
 int main() {
-    int rod = getpid();
-    printf("PID процесса: %d\n", rod);
-    int SIZE = 2048;
+    pid_t pid;
+    printf("PID %d: parent created\n", getpid());
     int fd;
     void *memory;
-    fd = shm_open(SHMNAME, O_CREAT | O_RDWR, 0777);
+    fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (fd < 0) {
-        printf("cant open file\n");
+        printf("PID %d: cant open shared memory\n", getpid());
         return -1;
     }
     ftruncate(fd, SIZE);
     memory = mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
-    int pid = fork();
-    if (pid == 0) {
-        signal(SIGUSR1, dochernij);
+    if ((pid = fork()) == 0) {
+        signal(SIGUSR1, handler);
         pause();
+        exit(0);
     } else {
-        char chislo[100];
-        printf("Введите числа: ");
-        scanf("%s", chislo);
-        strcat(memory, chislo);
-        printf("Родитель отправил: %s\n", (char *) memory);
+        char digits[6];
+        printf("PID %d: input 5 digits without spaces: ", getpid());
+        scanf("%s", digits);
+        strcat(memory, digits);
+        printf("PID %d: parent sent %s\n", getpid(), (char *) memory);
         kill(pid, SIGUSR1);
         waitpid(pid, NULL, 0);
-        printf("Родитель принял: %s\n", (char *) memory);
-        shm_unlink(SHMNAME);
+        printf("PID %d: parent received min and max: %s\n", getpid(), (char *) memory);
+        shm_unlink(SHM_NAME);
     }
     close(fd);
+    printf("PID %d: parent exited\n", getpid());
     exit(0);
 }
 
-void dochernij() {
-    int doch = getpid();
-    printf("PID процесса: %d\n", doch);
-    int SIZE = 1000;
+void handler() {
+    printf("PID %d: child created\n", getpid());
     int fd;
     void *memory;
-    fd = shm_open(SHMNAME, O_CREAT | O_RDWR, 0666);
+    fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     ftruncate(fd, SIZE);
     memory = mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
-    printf("Ребёнок принял: %s\n", (char *) memory);
-    int numbers = atoi(memory);
-
-    int max = 0, min = 99999;
-
+    printf("PID %d: child received %s\n", getpid(), (char *) memory);
+    int digits = atoi(memory);
+    int digit, minDigit = 9, maxDigit = 0;
     for (int i = 0; i < 5; i++) {
-        int digit = numbers % 10;
-        numbers /= 10;
-        if (digit < min) min = digit;
-        if (digit > max) max = digit;
+        digit = digits % 10;
+        digits /= 10;
+        if (digit < minDigit) minDigit = digit;
+        if (digit > maxDigit) maxDigit = digit;
     }
-
-    char rez[100];
-    sprintf(rez, "%d %d", max, min);
-    strcpy(memory, rez);
-    printf("Процесс %d отправил максимальное число: %d\n", getpid(), max);
-    printf("Процесс %d отправил минимальное число: %d\n", getpid(), min);
-    printf("Процесс %d завершается\n", getpid());
-
+    char values[4];
+    sprintf(values, "%d %d", minDigit, maxDigit);
+    strcpy(memory, values);
+    printf("PID %d: child sent min and max: %s\n", getpid(), (char *) memory);
+    printf("PID %d: child exited\n", getpid());
+    exit(0);
 }
