3978d0677905a1cb5bb37926d31f9917806a45fd
FalseR20 <kno3.2002@gmail.com>
2022-06-01 10:04:02 +0300

    feat(PL5): add task4

--- ProgLang/5/src/main.cpp
+++ ProgLang/5/src/main.cpp
@@ -1,34 +1,46 @@
 #include "listfunctions.cpp"
 #include "Money.h"
 #include "Vector.h"
+#include "pqfunctions.cpp"
+#include "queue"
+
 
 int main() {
-    list<double> values{1.0, 2.0, 6.0, 4.0, 5.0};
-    print(values);
-    replaceElement(values, 6.0, 3.0);
-    print(values);
-    deleteElement(values, 2.0);
-    print(values);
-    subDiff(values);
-    print(values);
+//    list<double> values{1.0, 2.0, 6.0, 4.0, 5.0};
+//    print(values);
+//    replaceElement(values, 6.0, 3.0);
+//    print(values);
+//    deleteElement(values, 2.0);
+//    print(values);
+//    subDiff(values);
+//    print(values);
+//
+//    list<Money> wallet{Money(53.31, "BYN"), Money(15, "USD"), Money(398.09, "RUB")};
+//    print(wallet);
+//    replaceElement(wallet, Money(15, "USD"), Money(30, "USD"));
+//    print(wallet);
+//    deleteElement(wallet, wallet.back());
+//    print(wallet);
+//    subDiff(wallet);
+//    print(wallet);
+//
+//    Vector<int> numbers = {2, 4, 3, 6, 8};
+//    numbers.print();
+//    numbers[4] = numbers[2];
+//    numbers.print();
+//    numbers.pop(2);
+//    numbers.print();
+//    numbers.subDiff();
+//    numbers.print();
 
-    list<Money> wallet{Money(53.31, "BYN"), Money(15, "USD"), Money(398.09, "RUB")};
-    print(wallet);
-    replaceElement(wallet, Money(15, "USD"), Money(30, "USD"));
-    print(wallet);
-    deleteElement(wallet, wallet.back());
-    print(wallet);
-    subDiff(wallet);
-    print(wallet);
-
-    Vector<int> numbers = {2, 4, 3, 6, 8};
-    numbers.print();
-    numbers[4] = numbers[2];
-    numbers.print();
-    numbers.pop(2);
-    numbers.print();
-    numbers.subDiff();
-    numbers.print();
-
-
+    priority_queue<int> pq;
+    vector<int> data{3, 5, 1, 4, 6, 2, 10};
+    for (auto el: data) pq.push(el);
+    print(pq);
+    pq = replaceElement(pq, 4, 8);
+    print(pq);
+    deleteElement(pq, 3);
+    print(pq);
+    subDiff(pq);
+    print(pq);
 }
---
+++ ProgLang/5/src/pqfunctions.cpp
@@ -0,0 +1,79 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+void print(priority_queue<T> pq) {
+    while (!pq.empty()) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << "\n";
+}
+template<class T>
+vector<T> &priorityQueueToVector(priority_queue<T> pq) {
+    auto vec = new vector<T>;
+    while (!pq.empty()) {
+        vec->push_back(pq.top());
+        pq.pop();
+    }
+    return *vec;
+}
+
+template<class T>
+priority_queue<T> &vectorToPriorityQueue(vector<T> vector) {
+    auto pq = new priority_queue<T>;
+    while (!vector.empty()) {
+        pq->push(vector.back());
+        vector.pop_back();
+    }
+    return *pq;
+}
+
+
+template<class T>
+priority_queue<T> & replaceElement(priority_queue<T> &pq, const T &oldValue, const T &newValue) {
+    auto vector = priorityQueueToVector(pq);
+    for(auto iter = vector.begin(); iter != vector.end(); iter++) {
+        if (*iter == oldValue) {
+            *iter = newValue;
+            break;
+        }
+    }
+    auto pq2 = new priority_queue<T>;
+    for (auto el:vector)
+        pq2->push(el);
+    return *pq2;
+}
+
+template<class T>
+void deleteElement(priority_queue<T> &pq, const T &oldValue) {
+    vector<T> vector;
+    while (pq.top() != oldValue && !pq.empty()) {
+        vector.push_back(pq.top());
+        pq.pop();
+    }
+    pq.pop();
+    while (!vector.empty()) {
+        pq.push(vector.back());
+        vector.pop_back();
+    }
+}
+
+template<class T>
+void subDiff(priority_queue<T> &pq) {
+    auto vector = priorityQueueToVector(pq);
+    if (vector.empty()) return;
+    auto iter = vector.begin();
+    auto minValue = *iter, maxValue = *iter;
+    for (iter++; iter != vector.end(); iter++) {
+        minValue = min(minValue, *iter);
+        maxValue = max(maxValue, *iter);
+    }
+    auto diff = maxValue - minValue;
+    for (iter = vector.begin(); iter != vector.end(); iter++)
+        *iter -= diff;
+    pq = vectorToPriorityQueue(vector);
+}
